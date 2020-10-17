171e4018cdafee42c9297e83f1d240c1ff2d8c98
crhuffer <crhuffer@gmail.com>
2020-10-17 11:55:39 -0400

    Bugfix: added the extension .csv to the output dataframes.

--- python/DataGenerator.py
+++ python/DataGenerator.py
@@ -1,6 +1,5 @@
 from sklearn.datasets import make_regression
 import pandas as pd
-import matplotlib.pyplot as plt
 
 import os
 
@@ -11,7 +10,6 @@ path_data = path_project + '/data/'
 path_rawdata = path_data + 'raw_data/'
 path_processeddata = path_data + 'processed_data/'
 path_datasetmetadata = path_data + 'datasetmetadata/'
-
 
 # %%
 
@@ -106,26 +104,10 @@ df1.head()
 
 # %%
 
-plt.imshow(df1.corr(), cmap='hot', interpolation='nearest')
-plt.imshow(df2.corr(), cmap='hot', interpolation='nearest')
-plt.imshow(df3.corr(), cmap='hot', interpolation='nearest')
-plt.imshow(df4.corr(), cmap='hot', interpolation='nearest')
-plt.imshow(df5.corr(), cmap='hot', interpolation='nearest')
-
-# %%
-
-df1.hist()
-df2.hist()
-df3.hist()
-df4.hist()
-df5.hist()
-
-# %%
-
-df1.to_csv(path_rawdata + 'df1')
-df2.to_csv(path_rawdata + 'df2')
-df3.to_csv(path_rawdata + 'df3')
-df4.to_csv(path_rawdata + 'df4')
-df5.to_csv(path_rawdata + 'df5')
+df1.to_csv(path_rawdata + 'df1.csv')
+df2.to_csv(path_rawdata + 'df2.csv')
+df3.to_csv(path_rawdata + 'df3.csv')
+df4.to_csv(path_rawdata + 'df4.csv')
+df5.to_csv(path_rawdata + 'df5.csv')
 
 # %%
